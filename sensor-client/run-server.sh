6fb21bbd36f7c5afdb7cbadd29b73311f7cd9ce6
Maximilian Müller <m8n.mueller@gmail.com>
2024-10-23 10:44:09 +0200

    Implement client with k6

--- sensor-client/run-server.sh
+++ sensor-client/run-server.sh
@@ -11,7 +11,7 @@ runner_dir=`dirname $runner`
 runner_exe="./$(basename $runner)"
 
 cd "$runner_dir"
-$runner_exe &
+$runner_exe > servers.log &
 runner_pid=$!
 echo "Started server with PID $runner_pid"
 
@@ -20,7 +20,10 @@ $tracker_exe $runner_pid &
 tracker_pid=$!
 echo "Started tracker with PID $tracker_pid"
 
-scala-cli run App.scala --main-class runner -- http://localhost:8044 $@ >> run-server.log
+sleep 2
+
+# we use the second (see shift) argument as virtual user count
+k6 run --out csv=sensorstats.csv script.js --env MY_VU_COUNT=$1
 
 echo "Done sending, waiting..."
 sleep 10
@@ -39,7 +42,7 @@ rm log.txt
 cd "$me_dir"
 tdir=`mktemp -d ./logs-XXXXXX`
 mv trace-$runner_pid.csv $tdir/
-mv sensorstats*.csv $tdir/
+mv sensorstats.csv $tdir/
 echo "$@" >> $tdir/config
 echo "Saved stats and trace to $tdir"
 
